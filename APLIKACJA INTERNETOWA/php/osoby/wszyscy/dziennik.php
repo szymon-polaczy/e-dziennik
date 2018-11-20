3df1ae61fceface7f8bcea81ed6044f4979e1575
Redzik <szymonpolaczy@wp.pl>
2018-11-20 20:06:24 +0100

    Dodanie menu do dziennik.php

--- APLIKACJA INTERNETOWA/php/osoby/wszyscy/dziennik.php
+++ APLIKACJA INTERNETOWA/php/osoby/wszyscy/dziennik.php
@@ -24,7 +24,33 @@
 </head>
 <body>
   <header>
-    <h1>STRONA GŁÓWNA PO LOGOWANIU</h1>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a href="dziennik.php" class="navbar-brand">BDG DZIENNIK</a>
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#glowneMenu">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div id="glowneMenu" class="collapse navbar-collapse">
+        <ul class="navbar-nav  ml-auto">
+          <?php
+            if ( $_SESSION['uprawnienia'] == "a") {
+              echo '<li class="nav-item"><a href="../admin/admin_klasy.php" class="nav-link">KLASY</a></li>';
+              echo '<li class="nav-item"><a href="../admin/admin_sale.php" class="nav-link">SALE</a></li>';
+              echo '<li class="nav-item"><a href="../admin/admin_przedmioty.php" class="nav-link">PRZEDMIOTY</a></li>';
+              echo '<li class="nav-item"><a href="../admin/admin_osoby.php" class="nav-link">OSOBY</a></li>';
+              echo '<li class="nav-item"><a href="../admin/admin_przydzialy.php" class="nav-link">PRZYDZIAŁY</a></li>';
+            } else if ( $_SESSION['uprawnienia'] == "n") {
+              echo '<li class="nav-item"><a href="../nauczyciel/wybierz_przydzial.php" class="nav-link">OCENY</a></li>';
+              echo '<li class="nav-item"><a href="../nauczyciel/nauczyciel_przydzialy.php" class="nav-link">PRZYDZIAŁY</a></li>';
+            } else if ( $_SESSION['uprawnienia'] == "u") {
+              echo '<li class="nav-item"><a href="../uczen/uczen_oceny.php" class="nav-link">OCENY</a></li>';
+              echo '<li class="nav-item"><a href="../uczen/uczen_przydzialy.php" class="nav-link">PRZYDZIAŁY</a></li>';
+            }
+            echo '<div class="dropdown-divider"></div>';
+            echo '<li class="nav-item"><a href="profil.php" class="nav-link">PROFIL</a></li>';
+          ?>
+        </ul>
+      </div>
+    </nav>
   </header>
 
   <main>
@@ -57,31 +83,6 @@
       ?>
     <div>
   </main>
-
-  <?php
-    if ( $_SESSION['uprawnienia'] == "a") {
-      echo '<h3>Rzeczy dostępne dla administratora</h3></br>';
-      echo '<a href="../admin/admin_klasy.php">KLASY</a></br>';
-      echo '<a href="../admin/admin_sale.php">SALE</a></br>';
-      echo '<a href="../admin/admin_przedmioty.php">PRZEDMIOTY</a></br>';
-      echo '<a href="../admin/admin_osoby.php">OSOBY</a></br>';
-      echo '<a href="../admin/admin_przydzialy.php">PRZYDZIAŁY</a>';
-
-      echo '</br></br></br></br></br></br>';
-    } else if ( $_SESSION['uprawnienia'] == "n") {
-      echo '<h3>Rzeczy dostępne dla nauczyciela</h3></br>';
-      echo '<a href="../nauczyciel/wybierz_przydzial.php">OCENY</a></br>';
-      echo '<a href="../nauczyciel/nauczyciel_przydzialy.php">ZOBACZ PRZYDZIAŁY</a>';
-
-      echo '</br></br></br></br></br></br>';
-    } else if ( $_SESSION['uprawnienia'] == "u") {
-      echo '<h3>Rzeczy dostępne dla ucznia</h3></br>';
-      echo '<a href="../uczen/uczen_oceny.php">ZOBACZ OCENY</a></br>';
-      echo '<a href="../uczen/uczen_przydzialy.php">ZOBACZ PRZYDZIAŁY</a>';
-
-      echo '</br></br></br></br></br></br>';
-    }
-  ?>
 
   <h3>Rzeczy dostępne dla wszystkich</h3></br>
   <a href="zmien_dane.php">ZMIEŃ DANE</a>
