73b54d89c39c7b389ac21ce1a52519b4ececf8a6
Szymon Polaczy <polaczyszymon@gmail.com>
2019-03-05 18:54:25 +0100

    Optymalizacja budowy plików
    Dodałem html-templates jak na razie dla headera na wszystkich stronach i naprawiłem link do zmien_dane.php

--- APLIKACJA INTERNETOWA/php/osoby/nauczyciel/nauczyciel_przydzialy.php
+++ APLIKACJA INTERNETOWA/php/osoby/nauczyciel/nauczyciel_przydzialy.php
@@ -44,55 +44,8 @@
   <link rel="stylesheet" href="../../../css/style.css">
 </head>
 <body class="index-body">
-  <header>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-      <a href="../wszyscy/dziennik.php" class="navbar-brand">BDG DZIENNIK</a>
-      <button class="navbar-toggler" data-toggle="collapse" data-target="#glowneMenu">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div id="glowneMenu" class="collapse navbar-collapse">
-        <ul class="navbar-nav  ml-auto">
-          <?php
-            if ( $_SESSION['uprawnienia'] == "a") {
-              echo '<li class="nav-item"><a href="../admin/admin_klasy.php" class="nav-link">KLASY</a></li>';
-              echo '<li class="nav-item"><a href="../admin/admin_sale.php" class="nav-link">SALE</a></li>';
-              echo '<li class="nav-item"><a href="../admin/admin_przedmioty.php" class="nav-link">PRZEDMIOTY</a></li>';
-              echo '<li class="nav-item"><a href="../admin/admin_osoby.php" class="nav-link">OSOBY</a></li>';
-              echo '<li class="nav-item"><a href="../admin/admin_przydzialy.php" class="nav-link">PRZYDZIAŁY</a></li>';
-            } else if ( $_SESSION['uprawnienia'] == "n") {
-              echo '<li class="nav-item"><a href="wybierz_przydzial.php" class="nav-link">OCENY</a></li>';
-              echo '<li class="nav-item"><a href="nauczyciel_przydzialy.php" class="nav-link">PRZYDZIAŁY</a></li>';
-            } else if ( $_SESSION['uprawnienia'] == "u") {
-              echo '<li class="nav-item"><a href="../uczen/uczen_oceny.php" class="nav-link">OCENY</a></li>';
-              echo '<li class="nav-item"><a href="../uczen/uczen_przydzialy.php" class="nav-link">PRZYDZIAŁY</a></li>';
-            }
-          ?>
-          <li class="nav-item">
-            <div class="dropdown">
-              <a href="#" class="nav-item btn btn-dark dropdown-toggle" role="button" id="dropdownProfil"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                PROFIL
-              </a>
-
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
-                <a class="dropdown-item disabled" href="#">Imie: <span class="wartosc"><?php echo $_SESSION['imie']; ?></span></a>
-                <a class="dropdown-item disabled" href="#">Nazwisko: <span class="wartosc"><?php echo $_SESSION['nazwisko']; ?></span></a>
-                <a class="dropdown-item disabled" href="#">Email: <span class="wartosc"><?php echo $_SESSION['email']; ?></span></a>
-                <?php
-                  if ($_SESSION['uprawnienia'] == "n")
-                    echo '<a class="dropdown-item disabled" href="#">Sala: <span class="wartosc">'.$_SESSION['sala_nazwa'].'</span></a>';
-                  else if ($_SESSION['uprawnienia'] == "u")
-                    echo '<a class="dropdown-item disabled" href="#">Klasa: <span class="wartosc">'.$_SESSION['klasa_nazwa'].'</span></a>';
-                ?>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="zmien_dane.php">ZMIEŃ DANE</a>
-                <a class="dropdown-item" href="../wszyscy/zadania/wyloguj.php">WYLOGUJ</a>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+  <!--HEADER INCLUDE-->
+  <?php include("../../../html-templates/after-login-header.php"); ?>
 
   <main>
     <section>
