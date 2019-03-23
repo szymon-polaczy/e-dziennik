d454bb240c8f81d2b60eb2b91120fd948f4726e7
Szymon Polaczy <polaczyszymon@gmail.com>
2019-03-23 11:41:01 +0100

    Dodanie informacji do inside-head.php template

--- APLIKACJA INTERNETOWA/html-templates/inside-head.php
+++ APLIKACJA INTERNETOWA/html-templates/inside-head.php
@@ -2,8 +2,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
 <title>E-DZIENNIK - <?php echo $title; ?></title>
-<meta name="keywords" content="">
-<meta name="description" content="">
+<meta name="keywords" content="e-dziennik, dziennik, elektroniczny, uczniowie, nauczyciele, administratorzy, dyrektorzy, aplikacja, internetowa, nauka, php, mysql, html, css, js">
+<meta name="description" content="Projekt dziennika elektronicznego tworzony przez ucznia dla uczniów do nauki budowania aplikacji internetowych.">
 <meta name="author" content="Szymon Polaczy">
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
