fdaccecdd07bdb059f57e9b598b0c40ce5a0ed2d
Szymon Polaczy <polaczyszymon@gmail.com>
2019-03-23 11:25:26 +0100

    Dodanie template środka head tagu
    Dodałem template środka head w html aby skrócić strony html i ułatwić mi poprawianie informacji

--- APLIKACJA INTERNETOWA/php/osoby/wszyscy/index.php
+++ APLIKACJA INTERNETOWA/php/osoby/wszyscy/index.php
@@ -10,16 +10,8 @@
 <!doctype html>
 <html lang="pl">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-
-  <title>E-DZIENNIK - Zaloguj Się</title>
-  <meta name="keywords" content="">
-  <meta name="description" content="">
-  <meta name="author" content="Szymon Polaczy">
-
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <link rel="stylesheet" href="../../../css/style.css">
+  <!--INSIDE OF HEAD INCLUDE-->
+  <?php $title = "Zaloguj się"; include("../../../html-templates/inside-head.php"); ?>
 </head>
 <body>
 
