3468bc4c633b392b7e0850baa8cf8bf6213b83ca
Szymon Polaczy <polaczyszymon@gmail.com>
2019-04-26 20:53:40 +0200

    I added some html templates for sign-in page

--- user-interactions/index.php
+++ user-interactions/index.php
@@ -3,6 +3,20 @@
   <?php $site_title = "Sign In"; include("templates/head-tag-inside.php"); ?>
 </head>
 <body>
-  
+  <head class="navigation-header">
+    <h1>school journal</h1>
+  </head>
+  <main>
+    <form action="sign-in.php" method="post">
+      <label for="email-input">Email</label>
+      <input id="email-input" name="email" type="email" placeholder="Email">
+      <label for="password-input">Password</label>
+      <input id="password-input" name="password" type="password" placeholder="Password">
+      <button type="submit">Sign In</button>
+    </form>
+  </main>
+  <footer>
+    <h6>Author: <a href="https://szymonpolaczy.pl">Szymon Polaczy</a></h6>
+  </footer>
 </body>
 </html>
