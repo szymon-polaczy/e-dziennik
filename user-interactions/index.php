2f5cf1e9b9ca1b504bf9f89f81014e824fcc2151
Szymon Polaczy <polaczyszymon@gmail.com>
2019-04-26 20:44:10 +0200

    I added first page and template for head tag

---
+++ user-interactions/index.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<head>
+  <?php $site_title = "Sign In"; include("templates/head-tag-inside.php"); ?>
+</head>
+<body>
+  
+</body>
+</html>
---
+++ user-interactions/templates/head-tag-inside.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="viewport" content="width=device-wiidth, initial-scale=1">
+
+<title>School Journal - <?php echo $site_title; ?></title>
+
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content="Szymon Polaczy">
