be6f6e81c715680c444d00b652088fa698e9d391
Szymon Polaczy <polaczyszymon@gmail.com>
2019-05-01 18:28:54 +0200

    I added connect file and checking if POST is right

---
+++ php-tasks/files-needed/connect.php
@@ -0,0 +1,5 @@
+<?php
+  $host = "localhost";
+  $db_user = "school-journal";
+  $db_password = "123qwerty";
+  $db_name = "school-journal";
--- php-tasks/sign_in.php
+++ php-tasks/sign_in.php
@@ -2,20 +2,27 @@
   session_start();
 
   require_once "../php-classes/user.php";
-
   require_once "../php-classes/pdo.php";
+  require_once "files-needed/connect.php";
+
+  if (!isset($_POST['email']) || !isset($_POST['password'])) {
+    header('Location: ../user-interactions/index.php');
+    exit();
+  }
 
   $class_user = new USER();
-  
-  $class_pdo_db = new PDO_DB('root', '<kizdeR<', 'school-journal');
+  $class_pdo_db = new PDO_DB($db_user, $db_password, $db_name, $host);
 
   $email = $_POST['email'];
   $password = $_POST['password'];
 
   $_SESSION['sign_in_message'] = $class_user->sign_in($class_pdo_db, $email, $password);
 
-  if ($_SESSION['sign_in_message'] == 0)
+  if ($_SESSION['sign_in_message'] == 0) {
+    unset($_SESSION['sign_in_message']);
     header('Location: ../user-interactions/journal.php');
-  else
+  }
+  else {
     header('Location: ../user-interactions/index.php');
+  }
   
