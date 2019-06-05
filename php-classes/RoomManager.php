d42dd2d8ec5af2125e505f26aae66475addcd704
Szymon Polaczy <polaczyszymon@gmail.com>
2019-06-05 10:20:18 +0200

    I added adding and deleting rooms to RoomManager

--- php-classes/RoomManager.php
+++ php-classes/RoomManager.php
@@ -2,7 +2,7 @@
 
   class RoomManager {
     private $pdo;
-    
+
     ########################################################
     # just a constructor
     # $pdo -> class pdo that I need to interact with database [PdoManager]
@@ -10,4 +10,54 @@
     public function __construct($pdo) {
       $this->pdo = $pdo;
     }
-  }
+
+    ########################################################
+    # adds new room to the database
+    # $name -> name of the new room [string]
+    ########################################################
+    public function add($name) {
+      $name = htmlentities($name, ENT_QUOTES, 'utf-8');
+
+      if (empty($name))
+        return "Name is required but it's empty.";
+
+      if (!is_string($name))
+        return "Name is not a valid text."; 
+
+      $sql = "SELECT id FROM room WHERE name='$name'";
+      $valid_name_response = $this->pdo->sqlTable($sql);
+      
+      if (count($valid_name_response) != 0)
+        return "Room with that name already exists.";
+
+      $sql = "INSERT INTO room VALUES (NULL, '$name')";
+      $response = $this->pdo->sqlQuery($sql);
+
+      if ($response > 0) {
+        return "Good.";
+      } else {
+        return "Room failed to be added.";
+      }
+    }
+
+    #########################################################
+    # deletes room in the database
+    # $id -> id of the room that you want to delete [number]
+    #########################################################
+    public function delete($id) {
+      if (empty($id))
+        return "Id is required but it's empty.";
+
+      if (!is_int($id))
+        return "Id is not a valid number.";
+
+      $sql = "DELETE FROM room WHERE id='$id'";
+      $response = $this->pdo->sqlQuery($sql);
+
+      if ($response > 0) {
+        return "Good.";
+      } else {
+        return "Room failed to be deleted.";
+      }
+    }
+  } 
