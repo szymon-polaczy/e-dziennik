330dd18a5973f907a5d00fbe18b9402d53499fad
Szymon Polaczy <polaczyszymon@gmail.com>
2019-06-12 10:38:57 +0200

    I added some form inputs for the users

--- user-interactions/users.php
+++ user-interactions/users.php
@@ -5,15 +5,23 @@
 
   require_once "../php-classes/PdoManager.php";
   require_once "../php-classes/UserManager.php";
+  require_once "../php-classes/ClassManager.php";
+  require_once "../php-classes/RoomManager.php";
 
   $pdo_manager = new PdoManager($db_user, $db_password, $db_name, $host);
   $user_manager = new UserManager();
+  $class_manager = new ClassManager($pdo_manager);
+  $room_manager = new RoomManager($pdo_manager);
+
+  $classes = $class_manager->getAll();
+  $rooms = $room_manager->getAll();
 
   if (!$user_manager->isSignedIn()) {
     header('Location: index.php');
   }
 ?>
 <!doctype html>
+<html lang="en">
 <?php $site_title = "Users"; include("templates/head_tag.php"); ?>
 <body>
   <?php include("templates/after_login_navigation_header.php"); ?>
@@ -35,10 +43,44 @@
           <input id="add-form-email" name="email" placeholder="Add your user email" type="text">
           <label for="add-form-password">Password</label>
           <input id="add-form-password" name="password" placeholder="Add your user password" type="text">
+          <label for="add-form-permission">Permission</label>
+          <select id="add-form-permission" name="permission">
+            <option></option>
+            <option value="a">Administrator</option>
+            <option value="t">Teacher</option>
+            <option value="s">Student</option>
+          </select>
+          <!--STUDENT HIDE BOX-->
+          <div id="add-form-student-hide-box">
+            <?php if(count($classes) == 0) : ?>
+              <small>There are no classes. To add a student you need to add classes first.</small>
+            <?php else : ?>
+              <label for="add-form-birthdate">Birthdate</label>
+              <input id="add-form-birthdate" name="birthdate" type="date">
+              <label for="add-form-class">Class</label>
+              <select id="add-form-class" name="class">
+                <option></option>
+                <?php foreach($classes as $class) : ?>
+                  <option value="<?php echo $class['id']; ?>"><?php echo $class['name']; ?></option>
+                <?php endforeach; ?>
+              </select>
+            <?php endif; ?>
+          </div>
+          <!--TEACHER HIDE BOX-->
+          <div id="add-form-teacher-hide-box">
+            <?php if(count($rooms) == 0) : ?>
+              <small>There are no rooms. To add a teacher you need to add rooms first.</small>
+            <?php else : ?>
+              <label for="add-form-room">Room</label>
+              <select id="add-form-room" name="room">
+                <option></option>
+                <?php foreach($rooms as $room) : ?>
+                  <option value="<?php echo $room['id']; ?>"><?php echo $room['name']; ?></option>
+                <?php endforeach; ?>
+              </select>
+            <?php endif; ?>
+          </div>
           <button type="submit">Add</button>
-          <!--PERMISSION-->
-          <!--STUDENT HIDE-->
-          <!--TEACHER HIDE-->
         </div>
       </form>
       <button id="btn-show-add-form"><i class="fas fa-plus"></i></button>
