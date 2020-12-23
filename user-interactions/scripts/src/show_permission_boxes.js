679000d52d347543a5137a1a391cfd5dec0a34e2
Szymon Polaczy <polaczyszymon@gmail.com>
2020-12-23 22:04:43 +0100

    Very small styling change and one js fix

--- user-interactions/scripts/src/show_permission_boxes.js
+++ user-interactions/scripts/src/show_permission_boxes.js
@@ -6,23 +6,25 @@ const add_form_class = document.getElementById("add-form-class");
 const add_form_birthday = document.getElementById("add-form-birthdate");
 const add_form_room = document.getElementById("add-form-room");
 
-add_form_permission.addEventListener("change", () => {
-  let value = add_form_permission.options[add_form_permission.selectedIndex].value;
+if (add_form_permission != null) {
+  add_form_permission.addEventListener("change", () => {
+    let value = add_form_permission.options[add_form_permission.selectedIndex].value;
 
-  student_hide_box.style.display = "none";
-  teacher_hide_box.style.display = "none";
+    student_hide_box.style.display = "none";
+    teacher_hide_box.style.display = "none";
 
-  add_form_class.required = false;
-  add_form_birthday.required = false;
-  add_form_room.required = false;
+    add_form_class.required = false;
+    add_form_birthday.required = false;
+    add_form_room.required = false;
 
-  if (value == "t") {
-    teacher_hide_box.style.display = "flex";
-    add_form_room.required = true;
-  }
-  else if (value == "s") {
-    student_hide_box.style.display = "flex";
-    add_form_class.required = true;
-    add_form_birthday.required = true;
-  }
-});
+    if (value == "t") {
+      teacher_hide_box.style.display = "flex";
+      add_form_room.required = true;
+    }
+    else if (value == "s") {
+      student_hide_box.style.display = "flex";
+      add_form_class.required = true;
+      add_form_birthday.required = true;
+    }
+  });
+}
